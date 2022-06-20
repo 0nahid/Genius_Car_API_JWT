02b7bf81e5b465b4337a3c0485db59c5c0cc7365
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-20 15:22:29 +0600

    added axios Privte

--- client/src/Pages/Checkout/Checkout/Checkout.js
+++ client/src/Pages/Checkout/Checkout/Checkout.js
@@ -1,4 +1,7 @@
+
 import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
@@ -7,43 +10,45 @@ import auth from '../../../firebase.init';
 import useServiceDetail from '../../../hooks/useServiceDetails';
 const Checkout = () => {
     const { id } = useParams();
-    const [service] = useServiceDetail(id);
+    // const [service] = useServiceDetail(id);
+    const [service, setService] = useState({})
+    useEffect(()=>{
+        const url = `http://localhost:5000/service/${id}`;
+        // console.log(url);
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setService(data));
+    },[id])
+    console.log(service.name + ' ' + service.price);
     const [user] = useAuthState(auth)
-    const { register, handleSubmit, reset } = useForm({
-        mode: 'onChange',
-        registerOnChange: false,
-        defaultValues: {
-            name: user?.name,
-            email: user?.email,
-            service: service.name,
-            amount: service.price,
-        }
-    });
+    const { register, handleSubmit, reset } = useForm();
     const onSubmit = data => {
-        axios.post(`http://localhost:5000/order`, data)
-            .then(res => {
-                (res.status === 200) ? toast.success('Order placed successfully') : toast.error('Order failed');
-                reset();
-            })
-
+        // axios.post('http://localhost:5000/order', data)
+        //     .then(res => {
+        //         (res.status === 200) ? toast.success('Order placed successfully') : toast.error('Order failed');
+        //         reset();
+        //     })
+        console.log(data);
     }
 
     return (
-        <div className="container mx-auto">
-            <h2>Place Order : {service?.name} </h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input className='input-group mb-2 p-2 rounded-sm ' value={user?.displayName} type="text" placeholder="Name" {...register("name", {})} />
-                <input className='input-group mb-2 p-2 rounded-sm ' value={user?.email} type="email" placeholder="Email" {...register("email", {})} />
-                <input required className='input-group mb-2 p-2 rounded-sm ' type="text" placeholder="Address" {...register("address", {})} />
-                <input className='input-group mb-2 p-2 rounded-sm ' type="text" value={service?.name} readOnly placeholder="Service" {...register("service", {})} />
-                <input required className='input-group mb-2 p-2 rounded-sm ' type="text" placeholder="Card Number" {...register("cardNo", {})} />
-                <input required className='input-group mb-2 p-2 rounded-sm ' type="text" placeholder="Expiry Date" {...register("expiryDate", {})} />
-                <input required className='input-group mb-2 p-2 rounded-sm ' type="text" placeholder="CVV" {...register("cvv", {})} />
-                <input className='input-group mb-2 p-2 rounded-sm ' value={service.price} type="text" placeholder="Amount" {...register("amount", {})} />
-                <input className='mb-2 p-2 rounded-sm ' type="submit" value="Submit" />
-            </form>
+        <div>
+            <h2>Please Checkout your booking</h2>
+            <div className="container mx-auto">
+                <h2>Place Order : {service?.name} </h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input className='input-group mb-2 p-2 rounded-sm ' value={user?.displayName} type="text" placeholder="Name" {...register("name", {})} />
+                    <input className='input-group mb-2 p-2 rounded-sm ' value={user?.email} type="email" placeholder="Email" {...register("email", {})} />
+                    <input required className='input-group mb-2 p-2 rounded-sm ' type="text" placeholder="Address" {...register("address", {})} />
+                    <input className='input-group mb-2 p-2 rounded-sm ' type="text" value={service?.name} readOnly placeholder="Service" {...register("service", {})} />
+                    <input required className='input-group mb-2 p-2 rounded-sm ' type="text" placeholder="Card Number" {...register("cardNo", {})} />
+                    <input required className='input-group mb-2 p-2 rounded-sm ' type="text" placeholder="Expiry Date" {...register("expiryDate", {})} />
+                    <input required className='input-group mb-2 p-2 rounded-sm ' type="text" placeholder="CVV" {...register("cvv", {})} />
+                    <input className='input-group mb-2 p-2 rounded-sm ' value={service.price} type="text" placeholder="Amount" {...register("amount", {})} />
+                    <input className='mb-2 p-2 rounded-sm ' type="submit" value="Submit" />
+                </form>
+            </div>
         </div>
     );
 };
-
 export default Checkout;
--- client/src/Pages/Order/Order.jsx
+++ client/src/Pages/Order/Order.jsx
@@ -1,28 +1,35 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from 'react-router-dom';
+import axiosPrivate from "../../api/axiosPrivate";
 import auth from "../../firebase.init";
-
 export default function Order() {
     const [orders, setOrders] = useState([])
     const [user] = useAuthState(auth)
-    useEffect(()=>{
+    const navigate = useNavigate()
+    useEffect(() => {
         const getOrders = async () => {
             const email = user.email;
             const url = `http://localhost:5000/order?email=${email}`
-            const { data } = await axios.get(url,{
-                headers: { 'authorization': `Bearer ${localStorage.getItem('accessToken')}` }
-            })
-            setOrders(data);
+            try {
+                const { data } = await axiosPrivate.get(url)
+                setOrders(data);
+            } catch (error) {
+                console.log(error?.message);
+                if (error?.response?.status === 401 || error?.response?.status === 403) {
+                    localStorage.removeItem('accessToken');
+                    navigate('/login');
+                }
+            }
         }
         getOrders();
-    },[user])
+    }, [user, navigate])
     return (
         <>
             <h2>Your Order:</h2>
             <ul>
                 {orders.map(order => (
-                    <li key={order.id}>
+                    <li key={order._id}>
                         {order.service} - {order.amount}$
                     </li>
                 ))}
